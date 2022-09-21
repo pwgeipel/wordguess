fb2894888cde08c35cf20abdfbad147919af601d
pwgeipel <108153968+pwgeipel@users.noreply.github.com>
2022-09-21 17:03:29 -0500

    renderCharacters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,34 @@
 var startBTN = document.querySelector('.start-button')
 var wordBlanksEl = document.querySelector('.word-blanks')
-// var validChars = "abcdefghijklmnopqrstuvwxyz"
+var validChars = "abcdefghijklmnopqrstuvwxyz"
 var words = ['pretzel', 'macarons', 'croissant', 'javascript', 'variable', 'python']
 var word  
+var guessedCharacters = []
 // score variable
 // time remaining variable
 
 function handleKeydown(event) {    
+    console.log(event.key)
     if (validChars.includes(event.key)) {
-        
-    }
+        guessedCharacters.push(event.key)
+        renderCharacters()
+
+    } 
 }
 // reset thje word-blanks - an _ for each letter of the new word
 function renderCharacters() {
     var str = ""
     for (var i = 0; i < word.length; i++) {
+        var letter = word[i]
+        if(guessedCharacters.includes(letter)) {
+            str += letter + ''
+        } else {
+            
+        
         str += "_ "
-        console.log(str)
+        }
     }
+    console.log(str)
     wordBlanksEl.textContent = str.trim()
 }
 
